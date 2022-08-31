b8e5217764c0b9fcd1e6b937ef3c7f714b11ce65
Harshit Rathi <harshitr2001@gmail.com>
2022-08-31 17:24:40 +0530

    basic setups

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './components/Home';
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <>
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={""} />  
+          <Route path="/" element={<Home/>} />  
       </Routes>
     </BrowserRouter>
     </>
